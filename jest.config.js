f01b4bf5782edacdb12428e93c7f9b9bf7c8f6d8
Mohamed Meabed <mo.meabed@gmail.com>
2025-12-25 14:49:04 -0500

    chore: update

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
-  testTimeout: 60000, // Increased to 60s for integration tests
+  testTimeout: 20000,
   testRegex: '__tests__/.*.test.ts$',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
   testEnvironment: 'node',
   collectCoverage: false,
   coverageReporters: ['json', 'html'],
@@ -11,31 +10,4 @@ module.exports = {
       tsconfig: 'tsconfig.test.json'
     }]
   },
-  // Add project-specific timeout configurations
-  projects: [
-    {
-      displayName: 'unit-tests',
-      testMatch: ['**/__tests__/*!(integration|smtp|socket)*.test.ts'],
-      testTimeout: 15000, // 15s for unit tests
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
-          tsconfig: 'tsconfig.test.json'
-        }]
-      }
-    },
-    {
-      displayName: 'integration-tests',
-      testMatch: ['**/__tests__/*(integration|smtp|socket)*.test.ts'],
-      testTimeout: 120000, // 2 minutes for integration tests
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
-          tsconfig: 'tsconfig.test.json'
-        }]
-      }
-    }
-  ]
 };
